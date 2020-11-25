6890cbb5aff4aa440b387f8ebd06735f86d36f76
Dynosaur <heracassia@gmail.com>
2020-11-25 09:50:12 -0600

    Using new payload, new actions

--- src/server/router/api-router.ts
+++ src/server/router/api-router.ts
@@ -1,63 +1,91 @@
-import { CreatePostEndpoint, GetPostEndpoint, GetPostsEndpoint,
-GetUserEndpoint } from '../interface/responses/api-responses';
-import { RouteInfo, RouteHandler } from '../route-handling/route-infra';
-import { payload } from '../route-handling/response-functions';
-import { convertDbUser } from '../../database/database-actions';
+import { convertDbPost, convertDbUser } from 'database/database-actions';
+import { payload, RouteInfo, RouteHandler, RoutePayload }
+from 'server/route-handling/route-infra';
+import { CreatePostCreatedResponse, CreatePostEndpoint, GetPostEndpoint,
+GetPostErrorResponse, GetPostFoundResponse, GetPostsEndpoint, GetUserEndpoint,
+GetUserErrorResponse, GetUserFoundResponse } from 'interface/responses/api-responses';
 
-export const getUser: RouteHandler<GetUserEndpoint> = async function getUser(request, actions) {
+export const getUser: RouteHandler<GetUserEndpoint> =
+async function getUser(request, actions): Promise<RoutePayload<GetUserEndpoint>> {
     const username = request.params.username;
-
-    const op = await actions.readUser(username);
-    if (op.status === 'NO_USER') {
-        return payload<GetUserEndpoint>(404, `Could not find user "${username}".`, {
-            status: 'NOT_FOUND'
-        });
+    const user = await actions.readUser(username);
+    if (user) {
+        return payload<GetUserFoundResponse>(
+            `Successfully found user "${username}".`,
+            200,
+            true,
+            { user: convertDbUser(user) }
+        );
     } else {
-        return payload<GetUserEndpoint>(200, `Successfully found user "${username}".`, {
-            status: 'OK', user: convertDbUser(op.data)
-        });
+        return payload<GetUserErrorResponse>(
+            `Could not find user "${username}".`,
+            404,
+            false,
+            { error: 'User Not Found' }
+        );
     }
 };
 
-export const getPosts: RouteHandler<GetPostsEndpoint> = async function getPosts(request, actions) {
-    const op = await actions.readPosts(request.query);
-
-    return payload<GetPostsEndpoint>(200, `Successfully found ${op.length} posts.`, {
-        status: 'OK', posts: op
-    });
+export const getPosts: RouteHandler<GetPostsEndpoint> =
+async function getPosts(request, actions): Promise<RoutePayload<GetPostsEndpoint>> {
+    const posts = await actions.readPosts(request.query);
+    return payload<GetPostsEndpoint>(
+        `Successfully found ${posts.length} posts.`,
+        200,
+        true,
+        { posts }
+    );
 };
 
-export const getPost: RouteHandler<GetPostEndpoint> = async function getPost(request, actions) {
-    const op = await actions.readPosts({ _id: request.params.id });
-    if (op.length) {
-        return payload<GetPostEndpoint>(200, `Found post ${request.params.id}.`, {
-            status: 'OK', post: op[0]
-        });
+export const getPost: RouteHandler<GetPostEndpoint> =
+async function getPost(request, actions): Promise<RoutePayload<GetPostEndpoint>> {
+    const post = await actions.readPosts({ _id: request.params.id });
+    if (post.length) {
+        return payload<GetPostFoundResponse>(
+            `Found post ${request.params.id}.`,
+            200,
+            true,
+            { post: post[0] }
+        );
     } else {
-        return payload<GetPostEndpoint>(404, `Could not find post ${request.params.id}.`, {
-            status: 'NOT_FOUND'
-        });
+        return payload<GetPostErrorResponse>(
+            `Could not find post ${request.params.id}.`,
+            404,
+            false,
+            { error: 'Post Not Found' }
+        );
     }
 };
 
-export const createPost: RouteHandler<CreatePostEndpoint> = async function createPost(request, actions) {
-    await actions.createPost(request.body.title, request.body.content, request.body.author);
-    return payload<CreatePostEndpoint>(201, 'Successfully created post.', {
-        status: 'CREATED'
-    });
+export const createPost: RouteHandler<CreatePostEndpoint> =
+async function createPost(request, actions): Promise<RoutePayload<CreatePostEndpoint>> {
+    const post = await actions.createPost(
+        request.body.title,
+        request.body.content,
+        request.body.author
+    );
+    return payload<CreatePostCreatedResponse>(
+        'Successfully created post.',
+        201,
+        true,
+        { post: convertDbPost(post) }
+    );
 };
 
 export const routes: RouteInfo[] = [
     { method: 'GET', path: '/api/user/:username', handler: getUser },
     { method: 'GET',  path: '/api/posts', handler: getPosts },
-    { method: 'POST', path: '/api/:username/post', handler: createPost, requirements: {
-        auth: {
-            checkParamUsername: true,
-            method: 'authenticate'
-        },
-        scope: {
-            body: { required: ['author', 'content', 'title'], replacements: [] }
+    {
+        method: 'POST', path: '/api/:username/post', handler: createPost,
+        requirements: {
+            auth: {
+                checkParamUsername: true,
+                method: 'authenticate'
+            },
+            scope: {
+                body: { required: ['author', 'content', 'title'], replacements: [] }
+            }
         }
-    } },
+    },
     { method: 'GET',  path: '/api/post/:id', handler: getPost }
 ];
