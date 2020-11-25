c7ff87d7cd670dffec64be044cdb79a223180506
Dynosaur <heracassia@gmail.com>
2020-11-25 09:39:32 -0600

    Removed unused code

--- src/server/route-handling/response-functions.ts
+++ src/server/route-handling/response-functions.ts
@@ -1,46 +1,30 @@
-import { RoutePayload } from './route-infra';
-import { routePayload } from './route-handler';
-import { AuthParseErrorResponse, InternalServerErrorResponse, MissingDataErrorResponse,
-UnauthorizedError, UnauthorizedErrorResponse, UserNoExistErrorResponse } from '../interface/responses/error-responses';
+import { payload, RoutePayload } from 'server/route-handling/route-infra';
+import { AuthParseErrorResponse, MissingDataErrorResponse, UnauthorizedErrorResponse }
+from 'interface/responses/error-responses';
 
-export function payload<T>(httpCode: number, message: string, payload: T): RoutePayload<T> {
-    return { httpCode, consoleMessage: message, payload };
-}
-
-export function couldNotParseRequest(thing: 'username' | 'password' | 'type'): RoutePayload<AuthParseErrorResponse> {
-    const message = `There was an error parsing the request's ${thing}.`;
-    return {
-        consoleMessage: message,
-        httpCode: 400,
-        payload: {
-            field: thing,
-            message,
-            status: 'E_AUTH_HEADER_PARSE'
-        }
-    };
+export function couldNotParseRequest(field: 'username' | 'password' | 'type'): RoutePayload<AuthParseErrorResponse> {
+    return payload<AuthParseErrorResponse>(`There was an error parsing the request's ${field}.`, 400, false, {
+        error: 'Authorization Header Parse',
+        field
+    });
 }
 
 export function missingData(possible: string[][], provided: object, name: string): RoutePayload<MissingDataErrorResponse> {
-    const consoleMessage = `Malformed request ${name}.
+    const message = `Malformed request ${name}.
     \n\tRequired: [${possible.join(' OR ')}]
     \n\tProvided: [${Object.keys(provided)}]`;
-    return routePayload<MissingDataErrorResponse>(400, consoleMessage, {
-        status: 'MISSING_DATA',
-        missing: { possible, provided: Object.keys(provided), scope: name }
+    return payload<MissingDataErrorResponse>(message, 400, false, {
+        error: 'Missing Requirements',
+        missing: {
+            possible,
+            provided: Object.keys(provided),
+            scope: name
+        },
     });
 }
 
-export function internalError(message: string): RoutePayload<InternalServerErrorResponse> {
-    return payload<InternalServerErrorResponse>(500, message, {
-        status: 'E_INTERNAL',
-        message
+export function unauthorized(): RoutePayload<UnauthorizedErrorResponse> {
+    return payload<UnauthorizedErrorResponse>('Authentication failed.', 401, false, {
+        error: 'Authorization Failed'
     });
-}
-
-export function unauthorized(why: UnauthorizedError): RoutePayload<UnauthorizedErrorResponse> {
-    return payload<UnauthorizedErrorResponse>(401, 'Authentication failed.', { status: why });
-}
-
-export function userDoesNotExist(): RoutePayload<UserNoExistErrorResponse> {
-    return payload<UserNoExistErrorResponse>(404, 'The user could not be found.', { status: 'E_USER_NO_EXIST' });
 }
