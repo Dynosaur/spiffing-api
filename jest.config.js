f726ede0de0f138e635be185d457f8cda57b06b2
Dynosaur <heracassia@gmail.com>
2020-11-25 08:34:32 -0600

    Added typescript paths

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
+const { compilerOptions } = require('./tsconfig');
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+
 module.exports = {
-    "roots": ["<rootDir>/tests"],
-    "testMatch": ["**/?(*.)+(spec|test).+(ts|tsx|js)"],
-    "transform": { "^.+\\.(ts|tsx)$": "ts-jest" },
-    "verbose": true
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+    roots: ['<rootDir>/tests'],
+    testMatch: ['**/?(*.)+(spec|test).+(ts|tsx|js)'],
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest'
+    },
+    verbose: true
 };
