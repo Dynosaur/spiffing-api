cceff45dc80d973587e5bfa767d43b25a7b31383
Dynosaur <heracassia@gmail.com>
2021-01-04 15:38:32 -0600

    All api-router handlers now use explicit promises, and they no longer use route info requirements

--- src/server/router/api-router.ts
+++ src/server/router/api-router.ts
@@ -1,131 +1,94 @@
 import { Post } from '../interface/data-types';
 import { ObjectId } from 'mongodb';
 import { BoundUser } from 'app/database/dbi/user-api';
+import { objectIdParseErrorMessage } from 'app/error-messages';
 import { payload, RouteInfo, RouteHandler, RoutePayload } from 'server/route-handling/route-infra';
 import { CreatePost, GetPost, GetPosts, GetUser, RatePost } from 'interface/responses/api-responses';
+import { getPostOrFail, createObjectIdOrFail, scopeMustHaveProps, getUserOrFail, authorizeOrFail } from '../route-handling/route-handler';
 
-export const getUser: RouteHandler<GetUser.Tx> = async function getUser(request, actions): Promise<RoutePayload<GetUser.Tx>> {
-    const unparsedId: string = request.params.id;
-    let parsedId: string | ObjectId;
-    if (unparsedId.length === 24) {
+export const getUser: RouteHandler<GetUser.Tx> = function getUser(request, actions): Promise<RoutePayload<GetUser.Tx>> {
+    return new Promise<RoutePayload<GetUser.Tx>>(async resolve => {
+        let id: ObjectId | string;
+        let user: BoundUser;
         try {
-            const uid = new ObjectId(unparsedId);
-            parsedId = uid;
-        } catch (e) {
-            parsedId = unparsedId;
+            id = new ObjectId(request.params.id);
+        } catch (error) {
+            if (error.message === objectIdParseErrorMessage)
+                id = request.params.id;
+            else throw error;
         }
-    } else parsedId = unparsedId;
-
-    let user: BoundUser;
-    if (typeof parsedId === 'string')
-        user = await actions.user.readUser({ username: parsedId });
-    else
-        user = await actions.user.readUser({ _id: parsedId });
-
-    if (user)
-        return payload<GetUser.Ok.UserFound>(`Successfully found user "${unparsedId}".`, 200, true, { user: user.toInterface() });
-    else {
-        const message = typeof parsedId === 'string' ? `Could not find user "${unparsedId}".` : `No user with the id of "${unparsedId}".`;
-        return payload<GetUser.Failed.UserNotFound>(message, 200, false, { error: 'User Not Found' });
-    }
-};
-
-export const getPosts: RouteHandler<GetPosts.Tx> = async function getPosts(request, actions): Promise<RoutePayload<GetPosts.Tx>> {
-    const allowedKeys: Array<keyof Post> = ['author', 'date', 'title'];
-    const allowed: string[] = [];
-    const blocked: string[] = [];
-    for (const queryKey of Object.keys(request.query)) {
-        if (!allowedKeys.includes(queryKey as any)) {
-            delete request.query[queryKey];
-            blocked.push(queryKey);
-        } else {
-            allowed.push(queryKey);
-            if (queryKey === 'author') {
-                try {
-                    request.query.author = new ObjectId(request.query.author as string) as any;
-                } catch (error) {
-                    if (error.message === 'Argument passed in must be a single String of 12 bytes or a string of 24 hex characters')
-                        return payload<GetPosts.Failed.AuthorParse>('Could not parse ID param', 200, false, {
-                            error: 'Author Parse',
-                            provided: request.query.author as string
-                        });
-                }
-            }
-        }
-    }
-    const posts = await actions.post.readPosts(request.query);
-    return payload<GetPosts.PostsFound>(`Successfully found ${posts.length} posts.`, 200, true, {
-        posts: posts.map(post => post.toInterface()),
-        ...allowed.length && { 'query-allowed': allowed },
-        ...blocked.length && { 'query-blocked': blocked }
+        user = await getUserOrFail(resolve, actions.user, id);
+        resolve(payload<GetUser.Success>(`Successfully found user "${request.params.id}".`, 200, true, { user: user.toInterface() }));
     });
 };
 
-export const getPost: RouteHandler<GetPost.Tx> = async function getPost(request, actions): Promise<RoutePayload<GetPost.Tx>> {
-    let _id: ObjectId;
-    try {
-        _id = new ObjectId(request.params.id);
-    } catch (error) {
-        if (error.message === 'Argument passed in must be a single String of 12 bytes or a string of 24 hex characters')
-            return payload<GetPost.Failed.IDParse>('Could not parse ID param', 200, false, { error: 'Could Not Parse ID' });
-        else throw error;
-    }
-    const post = await actions.post.readPosts({ _id });
-    if (post.length) return payload<GetPost.Ok.FoundPost>(`Found post ${request.params.id}.`, 200, true, { post: post[0].toInterface() });
-    else return payload<GetPost.Failed.NoPost>(`Could not find post ${request.params.id}.`, 200, false, { error: 'Post Not Found' });
-};
-
-export const createPost: RouteHandler<CreatePost.Tx> = async function createPost(request, actions): Promise<RoutePayload<CreatePost.Tx>> {
-    let postAuthor: ObjectId;
-    try {
-        postAuthor = new ObjectId(request.body.author);
-    } catch (e) {
-        return payload<CreatePost.Failed.Parse>(`Could not parse request author id "${request.body.author}" into ObjectId`, 400, false, {
-            error: 'Parsing Error',
-            path: { body: { author: request.body.author } }
+export const getPosts: RouteHandler<GetPosts.Tx> = function getPosts(request, actions): Promise<RoutePayload<GetPosts.Tx>> {
+    return new Promise<RoutePayload<GetPosts.Tx>>(async resolve => {
+        const allowedKeys: Array<keyof Post> = ['author', 'date', 'title'];
+        const allowed: string[] = [];
+        const blocked: string[] = [];
+        for (const queryKey of Object.keys(request.query)) {
+            if (!allowedKeys.includes(queryKey as any)) {
+                delete request.query[queryKey];
+                blocked.push(queryKey);
+            } else {
+                allowed.push(queryKey);
+                if (queryKey === 'author')
+                    request.query.author = createObjectIdOrFail(resolve, request.query.author as string) as any;
+            }
+        }
+        const posts = await actions.post.readPosts(request.query);
+        return payload<GetPosts.Success>(`Successfully found ${posts.length} posts.`, 200, true, {
+            posts: posts.map(post => post.toInterface()),
+            ...allowed.length && { 'query-allowed': allowed },
+            ...blocked.length && { 'query-blocked': blocked }
         });
-    }
-    const post = await actions.post.createPost(postAuthor, request.body.title, request.body.content);
-    return payload<CreatePost.Ok.Created>('Successfully created post.', 201, true, { post: post.toInterface() });
+    });
 };
 
-export const ratePost: RouteHandler<RatePost.Tx> = async function ratePost(request, actions): Promise<RoutePayload<RatePost.Tx>> {
-    const rating = Math.sign(request.body.rating);
-    const post = await actions.post.readPost(request.params.id);
-    if (post) {
-        if (rating === 1) await post.like();
-        else if (rating === -1) await post.dislike();
-        return payload<RatePost.Ok>(`Rated post ${request.params.id} with ${rating}`, 200, true, {});
-    } else {
-        return payload<RatePost.Failed.NoPost>(`Could not find post ${request.params.id}`, 200, false, { error: 'No Post' });
-    }
+export const getPost: RouteHandler<GetPost.Tx> = function getPost(request, actions): Promise<RoutePayload<GetPost.Tx>> {
+    return new Promise<RoutePayload<GetPost.Tx>>(async resolve => {
+        const id = createObjectIdOrFail(resolve, request.params.id);
+        const post = await getPostOrFail(resolve, actions.post, id);
+        resolve(payload<GetPost.Success>(`Found post ${request.params.id}.`, 200, true, { post: post[0].toInterface() }));
+    });
+};
+
+export const createPost: RouteHandler<CreatePost.Tx> = function createPost(request, actions): Promise<RoutePayload<CreatePost.Tx>> {
+    return new Promise<RoutePayload<CreatePost.Tx>>(async resolve => {
+        const user = await authorizeOrFail(resolve, actions.common, request.headers.authorization);
+        scopeMustHaveProps(resolve, request.body, 'body', ['title', 'content']);
+        const post = await actions.post.createPost(user._id, request.body.title, request.body.content);
+        resolve(payload<CreatePost.Success>('Successfully created post.', 201, true, { post: post.toInterface() }));
+    });
+};
+
+export const ratePost: RouteHandler<RatePost.Tx> = function ratePost(request, actions): Promise<RoutePayload<RatePost.Tx>> {
+    return new Promise<RoutePayload<RatePost.Tx>>(async resolve => {
+        scopeMustHaveProps(resolve, request.body, 'body', ['rating']);
+        const user = await authorizeOrFail(resolve, actions.common, request.headers.authorization);
+        const postId = createObjectIdOrFail(resolve, request.params.id);
+        const rating = Math.sign(request.body.rating);
+        const post = await getPostOrFail(resolve, actions.post, postId);
+        switch (rating) {
+            case -1:
+                await user.rate.dislikePost(post);
+                break;
+            case 0:
+                await user.rate.unratePost(post);
+                break;
+            case 1:
+                await user.rate.likePost(post);
+                break;
+        }
+        resolve(payload<RatePost.Success>(`Rated post ${request.params.id} with ${rating}`, 200, true, { }));
+    });
 };
 
 export const routes: RouteInfo[] = [
-    { method: 'GET', path: '/api/user/:id', handler: getUser },
-    { method: 'GET',  path: '/api/posts', handler: getPosts },
-    {
-        method: 'POST', path: '/api/post', handler: createPost,
-        requirements: {
-            auth: { method: 'authenticate' },
-            scope: {
-                body: { required: ['author', 'content', 'title'], replacements: [] }
-            }
-        }
-    },
-    { method: 'GET',  path: '/api/post/:id', handler: getPost },
-    {
-        handler: ratePost,
-        method: 'POST',
-        path: '/api/rate/post/:id',
-        requirements: {
-            auth: {
-                checkParamUsername: false,
-                method: 'authenticate'
-            },
-            scope: {
-                body: { required: ['rating'], replacements: [] }
-            }
-        }
-    }
+    { method: 'GET',  path: '/api/user/:id',      handler: getUser },
+    { method: 'GET',  path: '/api/posts',         handler: getPosts },
+    { method: 'POST', path: '/api/post',          handler: createPost },
+    { method: 'GET',  path: '/api/post/:id',      handler: getPost },
+    { method: 'POST', path: '/api/rate/post/:id', handler: ratePost }
 ];
