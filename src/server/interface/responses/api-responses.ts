77a0fe239b49fff4068280ef53cd063e81214775
Dynosaur <heracassia@gmail.com>
2021-01-04 10:52:34 -0600

    Shortened API responses

--- src/server/interface/responses/api-responses.ts
+++ src/server/interface/responses/api-responses.ts
@@ -1,89 +1,53 @@
-import { Automated } from './error-responses';
+import { OkResponse } from '../response';
 import { Post, User } from '../data-types';
-import { ErrorResponse, OkResponse } from '../response';
+import { IMissingDataError, INoPostFoundError, INoUserFoundError, IObjectIdParseError } from './error-responses';
 
 export namespace GetUser {
-    export namespace Failed {
-        export interface UserNotFound extends ErrorResponse<'User Not Found'> { }
-        export type Tx = UserNotFound;
+    export type ErrorTx = INoUserFoundError;
+
+    export interface Success extends OkResponse {
+        user: User;
     }
 
-    export namespace Ok {
-        export interface UserFound extends OkResponse {
-            user: User;
-        }
-        export type Tx = UserFound;
-    }
-
-    export type Tx = Failed.Tx | Ok.Tx;
-}
-
-export namespace GetPost {
-    export namespace Failed {
-        export interface NoPost extends ErrorResponse<'Post Not Found'> { }
-
-        export interface IDParse extends ErrorResponse<'Could Not Parse ID'> { }
-
-        export type Tx = IDParse | NoPost;
-    }
-
-    export namespace Ok {
-        export interface FoundPost extends OkResponse {
-            post: Post;
-        }
-
-        export type Tx = FoundPost;
-    }
-
-    export type Tx = Failed.Tx | Ok.Tx;
+    export type Tx = ErrorTx | Success;
 }
 
 export namespace GetPosts {
-    export namespace Failed {
-        export interface AuthorParse extends ErrorResponse<'Author Parse'> {
-            provided: string;
-        }
+    export type ErrorTx = INoPostFoundError | IObjectIdParseError;
 
-        export type Tx = AuthorParse;
-    }
-
-    export interface PostsFound extends OkResponse {
+    export interface Success extends OkResponse {
         posts: Post[];
         'query-blocked'?: string[];
         'query-allowed'?: string[];
     }
 
-    export type Tx = Failed.Tx | PostsFound;
+    export type Tx = ErrorTx | Success;
+}
+
+export namespace GetPost {
+    export type ErrorTx = INoPostFoundError | IObjectIdParseError;
+
+    export interface Success extends OkResponse {
+        post: Post;
+    }
+
+    export type Tx = ErrorTx | Success;
 }
 
 export namespace CreatePost {
-    export namespace Failed {
-        export interface Parse extends ErrorResponse<'Parsing Error'> {
-            path: object;
-        }
+    export type ErrorTx = IMissingDataError | IObjectIdParseError;
 
-        export type Tx = Automated.Tx | Parse;
+    export interface Success extends OkResponse {
+        post: Post;
     }
 
-    export namespace Ok {
-        export interface Created extends OkResponse {
-            post: Post;
-        }
-
-        export type Tx = Created;
-    }
-
-    export type Tx = Failed.Tx | Ok.Tx;
+    export type Tx = ErrorTx | Success;
 }
 
 export namespace RatePost {
-    export namespace Failed {
-        export interface NoPost extends ErrorResponse<'No Post'> { }
+    export type ErrorTx = IMissingDataError | INoPostFoundError | IObjectIdParseError;
 
-        export type Tx = Automated.Tx | NoPost;
-    }
+    export interface Success extends OkResponse { }
 
-    export interface Ok extends OkResponse { }
-
-    export type Tx = Failed.Tx | Ok;
+    export type Tx = ErrorTx | Success;
 }
