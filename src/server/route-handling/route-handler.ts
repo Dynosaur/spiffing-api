56ec409a9b7ece2de1c0ea1ad9e339c67c8558ed
Dynosaur <heracassia@gmail.com>
2021-01-05 11:45:30 -0600

    Removed checkScope, added requestMustBeAuthenticated

--- src/server/route-handling/route-handler.ts
+++ src/server/route-handling/route-handler.ts
@@ -1,24 +1,22 @@
 import { chalk } from 'tools/chalk';
 import { Request } from 'express';
 import { ObjectId } from 'mongodb';
-import { checkScope } from 'server/route-handling/check-scope';
 import { CommonActions } from 'app/database/common-actions';
 import { DatabaseActions } from 'server/route-handling/route-infra';
 import { decodeBasicAuth } from 'tools/auth';
 import { BoundUser, UserAPI } from 'app/database/dbi/user-api';
 import { BoundPost, PostAPI } from 'app/database/dbi/post-actions';
 import { objectIdParseErrorMessage } from 'app/error-messages';
+import { RoutePayload, RouteHandler } from 'server/route-handling/route-infra';
 import { ErrorResponse as IErrorResponse } from 'interface/response';
-import { RoutePayload, RouteHandler, RouteHandlerRequirements } from 'server/route-handling/route-infra';
-import { AuthorizedRequestError, IMissingDataError, INoPostFoundError, INoUserFoundError, IObjectIdParseError } from 'interface/responses/error-responses';
 import { MissingDataError, NoPostFoundError, NoUserFoundError, ObjectIdParseError, UnauthenticatedError, UnauthorizedError } from '../interface-bindings/error-responses';
+import { AuthorizedRequestError, IMissingDataError, INoPostFoundError, INoUserFoundError, IObjectIdParseError, IUnauthenticatedError } from 'interface/responses/error-responses';
 
 export async function executeRouteHandler(
     request: Request,
     actions: DatabaseActions,
     handler: RouteHandler<any>,
     fingerprint: string,
-    requirements?: RouteHandlerRequirements,
     verbose = true): Promise<void> {
     function sendPayload(request: Request, payload: RoutePayload<any>, verbose = true): void {
         request.res.status(payload.httpCode).send(payload.payload);
@@ -31,35 +29,13 @@ export async function executeRouteHandler(
             }
         }
     }
-
-    const routeHandlerArgs: any = {};
-
-    if (requirements) {
-        if (verbose) {
-            chalk.sky(`${fingerprint} Checking route handler ${handler.name} requirements.`);
-        }
-        const presentHeaders: any = {};
-        if (requirements.scope) {
-            for (const scope of Object.keys(requirements.scope)) {
-                const check = checkScope(requirements.scope[scope].required, requirements.scope[scope].replacements, request[scope], scope);
-                if (check) {
-                    sendPayload(request, check, verbose);
-                    return;
-                }
-                presentHeaders[scope] = true;
-            }
-        }
-    } else if (verbose) {
-        chalk.lime(`${fingerprint} Route handler has no requirements.`);
-    }
-
     if (verbose) {
         chalk.sky(`${fingerprint} Executing route handler "${handler.name}".`);
     }
 
     let routePayload: RoutePayload<any>;
     try {
-        routePayload = await handler(request, actions, routeHandlerArgs);
+        routePayload = await handler(request, actions);
     } catch (error) {
         if (verbose) chalk.red(`ERROR: route handler "${handler.name}" threw an error: ${error.message}`);
         if (error.message === 'Topology is closed, please connect') {
@@ -128,14 +104,18 @@ export async function getUserOrFail(resolve: ERes<INoUserFoundError>, userAPI: U
     return user;
 }
 
-export async function authorizeOrFail(resolve: ERes<AuthorizedRequestError>, commonAPI: CommonActions, authHeader: string): Promise<BoundUser> {
-    if (!authHeader)
-        resolve(new UnauthenticatedError().toRoutePayload());
+export async function authorizeOrFail(resolve: ERes<AuthorizedRequestError>, commonAPI: CommonActions, authorizationHeader: string): Promise<BoundUser> {
+    requestMustBeAuthenticated(resolve, authorizationHeader);
 
-    const decoded = decodeBasicAuth(resolve, authHeader);
+    const decoded = decodeBasicAuth(resolve, authorizationHeader);
     const user = await commonAPI.authorize(decoded.username, decoded.password);
     if (!user)
         resolve(new UnauthorizedError().toRoutePayload());
 
     return user;
+}
+
+export function requestMustBeAuthenticated(resolve: ERes<IUnauthenticatedError>, authorizationHeader: string): void {
+    if (!authorizationHeader)
+        resolve(new UnauthenticatedError().toRoutePayload());
 }
