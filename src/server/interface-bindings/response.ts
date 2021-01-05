d4657527ab66972867cd776ebc677f49fa502f52
Dynosaur <heracassia@gmail.com>
2021-01-05 11:43:58 -0600

    Moved toRoutePayload to BaseResponse, made generic, and added message and code to BaseResponse

--- src/server/interface-bindings/response.ts
+++ src/server/interface-bindings/response.ts
@@ -1,43 +1,41 @@
 import { RoutePayload } from 'app/server/route-handling/route-infra';
 import { BaseResponse as IBaseResponse, OkResponse as IOkResponse, ErrorResponse as IErrorResponse } from 'interface/response';
 
-export class BaseResponse {
-    public payload: IBaseResponse;
+export class BaseResponse<PayloadType extends IBaseResponse> {
+    public payload: PayloadType;
 
-    constructor(public ok: boolean) {
-        this.payload = { ok: this.ok };
-    }
-}
-
-export class OkResponse extends BaseResponse {
-    public ok: true;
-    public payload: IOkResponse;
-
-    constructor() {
-        super(true);
-        this.payload = { ok: true };
-    }
-}
-
-export class ErrorResponse<ErrorInterface extends IErrorResponse<string>> extends BaseResponse {
-    public ok: false;
-    public payload: ErrorInterface;
-
-    constructor(public error: ErrorInterface['error'], public message: string, public eCode: number) {
-        super(false);
-        this.payload = {
-            error: this.error,
-            ok: false
-        } as any;
+    constructor(public ok: boolean, public message: string, public code = 200) {
+        this.payload.ok = this.ok;
     }
 
-    toRoutePayload(): RoutePayload<ErrorInterface> {
+    toRoutePayload(): RoutePayload<PayloadType> {
         return {
             consoleMessage: this.message,
-            httpCode: this.eCode,
+            httpCode: this.code,
             payload: {
                 ...this.payload
             }
         };
+    }
+}
+
+export class OkResponse<PayloadType extends IOkResponse> extends BaseResponse<PayloadType> {
+    public ok: true;
+    public payload: PayloadType;
+
+    constructor(message: string, code = 200) {
+        super(true, message, code);
+        this.payload.ok = true;
+    }
+}
+
+export class ErrorResponse<ErrorInterface extends IErrorResponse<string>> extends BaseResponse<ErrorInterface> {
+    public ok: false;
+    public payload: ErrorInterface;
+
+    constructor(public error: ErrorInterface['error'], message: string, code = 200) {
+        super(false, message, code);
+        this.payload.error = this.error;
+        this.payload.ok = false;
     }
 }
