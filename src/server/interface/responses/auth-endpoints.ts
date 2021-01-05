b0259fac06fcb05eaf2a08a6d46f60fdab2ec1e7
Dynosaur <heracassia@gmail.com>
2021-01-05 11:41:20 -0600

    Refactored auth endpoints

--- src/server/interface/responses/auth-endpoints.ts
+++ src/server/interface/responses/auth-endpoints.ts
@@ -1,52 +1,44 @@
 import { User } from '../data-types';
-import { Automated } from './error-responses';
 import { ErrorResponse, OkResponse } from '../response';
+import { AuthorizedRequestError, IAuthHeaderIdParamMismatchError, IAuthorizationParseError,
+INoUserFoundError, IObjectIdParseError, IUnauthenticatedError } from './error-responses';
 
-export namespace Register {
+export namespace IRegister {
     export namespace Failed {
         export interface UserExists extends ErrorResponse<'User Already Exists'> { }
-        export type Tx = Automated.Tx | UserExists;
+        export type Tx = IAuthorizationParseError | IUnauthenticatedError | UserExists;
     }
 
-    export namespace Ok {
-        export interface Created extends OkResponse {
-            user: User;
-        }
-        export type Tx = Created;
+    export interface Success extends OkResponse {
+        user: User;
     }
 
-    export type Tx = Failed.Tx | Ok.Tx;
+    export type Tx = Failed.Tx | Success;
 }
 
-export namespace Authenticate {
-    export type Failed = Automated.Tx;
-    export type Ok = OkResponse;
-    export type Tx = Failed | Ok;
+export namespace IAuthorize {
+    export type ErrTx = AuthorizedRequestError | IAuthHeaderIdParamMismatchError;
+
+    export interface Success extends OkResponse { }
+
+    export type Tx = ErrTx | Success;
 }
 
-export namespace Deregister {
-    export namespace Failed {
-        export interface NoUser extends ErrorResponse<'No User'> { }
-        export type Tx = Automated.Tx | NoUser;
-    }
+export namespace IDeregister {
+    export type ErrTx = AuthorizedRequestError | IAuthHeaderIdParamMismatchError | INoUserFoundError | IObjectIdParseError;
 
-    export type Ok = OkResponse;
+    export interface Success extends OkResponse { }
 
-    export type Tx = Failed.Tx | Ok;
+    export type Tx = ErrTx | Success;
 }
 
-export namespace Patch {
-    export namespace Failed {
-        export interface NoUser extends ErrorResponse<'No User'> { }
-        export type Tx = Automated.Tx | NoUser;
+export namespace IPatch {
+    export type ErrTx =  IAuthHeaderIdParamMismatchError | AuthorizedRequestError | INoUserFoundError;
+
+    export interface Success extends OkResponse {
+        updated: string[];
+        'rejected-props': string[];
     }
 
-    export namespace Ok {
-        export interface Updated extends OkResponse {
-            updated: string[];
-        }
-        export type Tx = Updated;
-    }
-
-    export type Tx = Failed.Tx | Ok.Tx;
+    export type Tx = ErrTx | Success;
 }
