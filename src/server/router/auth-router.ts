417ef958e26c2eb48dbc3806f0e8508fc66ee82b
Dynosaur <heracassia@gmail.com>
2021-01-06 11:25:53 -0600

    Removed promise based auth route handlers

--- src/server/router/auth-router.ts
+++ src/server/router/auth-router.ts
@@ -2,75 +2,75 @@ import { decodeBasicAuth } from 'app/tools/auth';
 import { RouteInfo, RouteHandler, RoutePayload } from 'server/route-handling/route-infra';
 import { Authorize, Deregister, Patch, Register } from '../interface-bindings/auth-responses';
 import { IAuthorize, IDeregister, IPatch, IRegister } from 'interface/responses/auth-endpoints';
-import { authorizeOrFail, requestMustBeAuthenticated } from '../route-handling/route-handler';
-import { AuthHeaderIdParamError, NoUserFoundError, UnauthorizedError } from '../interface-bindings/error-responses';
+import { AuthHeaderIdParamError, UnauthenticatedError, UnauthorizedError } from '../interface-bindings/error-responses';
 
-export const register: RouteHandler<IRegister.Tx> = function register(request, actions): Promise<RoutePayload<IRegister.Tx>> {
-    return new Promise<RoutePayload<IRegister.Tx>>(async resolve => {
-        requestMustBeAuthenticated(resolve, request.headers.authorization);
-        const userInfo = decodeBasicAuth(resolve, request.headers.authorization);
-        if (await actions.user.readUser({ username: userInfo.username }))
-            resolve(new Register.UserExistsError(userInfo.username).toRoutePayload());
-        const user = await actions.user.createUser(userInfo.username, actions.common.securePassword(userInfo.password));
-        resolve(new Register.Success(user.toInterface()).toRoutePayload());
-    });
+export const register: RouteHandler<IRegister.Tx> = async function register(request, actions): Promise<RoutePayload<IRegister.Tx>> {
+        if (!request.headers.authorization) return new UnauthenticatedError();
+
+        const decodeAttempt = decodeBasicAuth(request.headers.authorization);
+        if (decodeAttempt instanceof RoutePayload) return decodeAttempt;
+
+        if (await actions.user.readUser({ username: decodeAttempt.username }))
+            return new Register.UserExistsError(decodeAttempt.username);
+
+        const user = await actions.user.createUser(
+            decodeAttempt.username,
+            actions.common.securePassword(decodeAttempt.password)
+        );
+        return new Register.Success(user.toInterface());
 };
 
-export const authorize: RouteHandler<IAuthorize.Tx> = function authenticate(request, actions): Promise<RoutePayload<IAuthorize.Tx>> {
-    return new Promise<RoutePayload<IAuthorize.Tx>>(async resolve => {
-        const user = await authorizeOrFail(resolve, actions.common, request.headers.authorization);
-        if (user.id !== request.params.id)
-            resolve(new AuthHeaderIdParamError(user.id, request.params.id).toRoutePayload());
-        resolve(new Authorize.Success(user).toRoutePayload());
-    });
+export const authorize: RouteHandler<IAuthorize.Tx> = async function authenticate(request, actions): Promise<RoutePayload<IAuthorize.Tx>> {
+    if (!request.headers.authorization) return new UnauthenticatedError();
+
+    const decodeAttempt = decodeBasicAuth(request.headers.authorization);
+    if (decodeAttempt instanceof RoutePayload) return decodeAttempt;
+
+    const user = await actions.common.authorize(decodeAttempt.username, decodeAttempt.password);
+    if (!user) return new UnauthorizedError();
+    if (user.id !== request.params.id) return new AuthHeaderIdParamError(user.id, request.params.id);
+
+    return new Authorize.Success(user);
 };
 
-export const deregister: RouteHandler<IDeregister.Tx> = function deregister(request, actions): Promise<RoutePayload<IDeregister.Tx>> {
-    return new Promise<RoutePayload<IDeregister.Tx>>(async resolve => {
-        requestMustBeAuthenticated(resolve, request.headers.authorization);
+export const deregister: RouteHandler<IDeregister.Tx> = async function deregister(request, actions): Promise<RoutePayload<IDeregister.Tx>> {
+    if (!request.headers.authorization) return new UnauthenticatedError();
 
-        const decoded = decodeBasicAuth(resolve, request.headers.authorization);
-        if (!await actions.user.readUser({ username: decoded.username }))
-            resolve(new NoUserFoundError(decoded.username).toRoutePayload());
+    const decodeAttempt = decodeBasicAuth(request.headers.authorization);
+    if (decodeAttempt instanceof RoutePayload) return decodeAttempt;
+    const user = await actions.common.authorize(decodeAttempt.username, decodeAttempt.password);
+    if (!user) return new UnauthorizedError();
+    if (user.id !== request.params.id) return new AuthHeaderIdParamError(user.id, request.params.id);
 
-        const user = await actions.common.authorize(decoded.username, decoded.password);
-        if (!user)
-            resolve(new UnauthorizedError().toRoutePayload());
-
-        if (user.id !== request.params.id)
-            resolve(new AuthHeaderIdParamError(user.id, request.params.id).toRoutePayload());
-
-        await user.delete();
-
-        resolve(new Deregister.Success(user).toRoutePayload());
-    });
+    await user.delete();
+    return new Deregister.Success(user);
 };
 
 export const patchUser: RouteHandler<IPatch.Tx> = async function patchUser(request, actions): Promise<RoutePayload<IPatch.Tx>> {
-    return new Promise<RoutePayload<IPatch.Tx>>(async resolve => {
-        const user = await authorizeOrFail(resolve, actions.common, request.headers.authorization);
-        if (user.id !== request.params.id)
-            resolve(new AuthHeaderIdParamError(user.id, request.params.id).toRoutePayload());
+    if (!request.headers.authorization) throw new UnauthenticatedError();
 
-        const updated: string[] = [];
-        const rejected: string[] = [];
+    const decodeAttempt = decodeBasicAuth(request.headers.authorization);
+    if (decodeAttempt instanceof RoutePayload) return decodeAttempt;
+    const user = await actions.common.authorize(decodeAttempt.username, decodeAttempt.password);
+    if (user.id !== request.params.id) return new AuthHeaderIdParamError(user.id, request.params.id);
 
-        for (const key of Object.keys(request.body)) {
-            if (key === 'password') {
-                await user.update({ password: actions.common.securePassword(request.body.password) });
-                updated.push('password');
-            } else if (key === 'screenname') {
-                await user.update({ screenname: request.body.screenname });
-                updated.push('screenname');
-            } else if (key === 'username') {
-                await user.update({ username: request.body.username });
-                updated.push('username');
-            } else
-                rejected.push(key);
-        }
+    const updated: string[] = [];
+    const rejected: string[] = [];
 
-        resolve(new Patch.Success(user, updated, rejected).toRoutePayload());
-    });
+    for (const key of Object.keys(request.body))
+        if (key === 'password') {
+            await user.update({ password: actions.common.securePassword(request.body.password) });
+            updated.push('password');
+        } else if (key === 'screenname') {
+            await user.update({ screenname: request.body.screenname });
+            updated.push('screenname');
+        } else if (key === 'username') {
+            await user.update({ username: request.body.username });
+            updated.push('username');
+        } else
+            rejected.push(key);
+
+    return new Patch.Success(user, updated, rejected);
 };
 
 export const routes: RouteInfo[] = [
