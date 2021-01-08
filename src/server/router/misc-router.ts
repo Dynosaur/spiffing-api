aa0b2cbb6003975230b784fc6a2bd69ceadb99c5
Dynosaur <heracassia@gmail.com>
2021-01-08 13:39:09 -0600

    Using RoutePayload

--- src/server/router/misc-router.ts
+++ src/server/router/misc-router.ts
@@ -1,13 +1,11 @@
-import { RouteInfo } from 'server/route-handling/route-infra';
+import { RouteInfo, RoutePayload } from 'server/route-handling/route-infra';
 
 export const indexRoute: RouteInfo = {
     handler: async () => {
-        return {
-            consoleMessage: 'Pinged index page.',
-            httpCode: 200,
-            ok: true,
-            payload: { message: 'Hello!' }
-        };
+        return new RoutePayload('Pinged index page.', {
+            message: 'Hello!',
+            ok: true
+        });
     },
     method: 'GET',
     path: '/'
