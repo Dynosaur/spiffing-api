8fbfe8b2fa4d0709edf90ce4fa2922d4dbfa8d2a
Dynosaur <heracassia@gmail.com>
2021-01-08 13:18:54 -0600

    BaseResponse now extends RoutePayload, as instanceof did not work before

--- src/server/interface-bindings/response.ts
+++ src/server/interface-bindings/response.ts
@@ -1,10 +1,12 @@
+import { RoutePayload } from 'route-handling/route-infra';
 import { IBaseResponse as IBaseResponse, IOkResponse as IOkResponse, IErrorResponse as IErrorResponse } from 'interface/response';
 
-export class BaseResponse<PayloadType extends IBaseResponse> {
+export class BaseResponse<PayloadType extends IBaseResponse> extends RoutePayload<PayloadType> {
     public payload: PayloadType;
 
-    constructor(public ok: boolean, public message: string, public code = 200) {
-        this.payload.ok = this.ok;
+    constructor(public ok: boolean, message: string, code = 200) {
+        super(message, {} as any, code);
+        this.payload.ok = ok;
     }
 }
 
