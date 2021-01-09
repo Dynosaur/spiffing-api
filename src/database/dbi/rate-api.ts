f95d7eb9d91fcad935db7e7fed8505efb692db7c
Dynosaur <heracassia@gmail.com>
2021-01-08 23:43:07 -0600

    Fixed bugs found during testing

--- src/database/dbi/rate-api.ts
+++ src/database/dbi/rate-api.ts
@@ -9,18 +9,18 @@ export class RateAPI {
     constructor(private dbi: DatabaseInterface<DbRatedPosts>, private uid: ObjectId) { }
 
     async initialize(): Promise<void> {
-        this.userRatedPosts = await this.dbi.read({ owner: this.uid })[0];
-        if (this.userRatedPosts === undefined || this.userRatedPosts === null) {
+        const result = await this.dbi.read({ owner: this.uid });
+        if (result.length === 0) {
             this.userRatedPosts = {
                 _id: new ObjectId(),
                 owner: this.uid,
                 posts: []
             };
             await this.dbi.create(this.userRatedPosts);
-        }
+        } else this.userRatedPosts = result[0];
     }
 
-    async likePost(post: BoundPost): Promise<void> {
+    async likePost(post: BoundPost): Promise<boolean> {
         const ratedPost = this.userRatedPosts.posts.find(p => p._id.toHexString() === post.id);
         if (ratedPost) {
             if (ratedPost.rating === -1) {
@@ -28,7 +28,9 @@ export class RateAPI {
                 post.setLikes(post.getLikes() + 1);
                 ratedPost.rating = 1;
                 await post.flush();
-            }
+                await this.dbi.update({ owner: this.uid }, { posts: this.userRatedPosts.posts });
+                return true;
+            } else return false;
         } else {
             this.userRatedPosts.posts.push({
                 _id: post.getId(),
@@ -37,10 +39,11 @@ export class RateAPI {
             await this.dbi.update({ owner: this.uid }, { posts: this.userRatedPosts.posts });
             post.setLikes(post.getLikes() + 1);
             await post.flush();
+            return true;
         }
     }
 
-    async dislikePost(post: BoundPost): Promise<void> {
+    async dislikePost(post: BoundPost): Promise<boolean> {
         const ratedPost = this.userRatedPosts.posts.find(p => p._id.toHexString() === post.id);
         if (ratedPost) {
             if (ratedPost.rating === 1) {
@@ -48,7 +51,9 @@ export class RateAPI {
                 post.setDislikes(post.getDislikes() + 1);
                 ratedPost.rating = -1;
                 await post.flush();
-            }
+                await this.dbi.update({ owner: this.uid }, { posts: this.userRatedPosts.posts });
+                return true;
+            } else return false;
         } else {
             this.userRatedPosts.posts.push({
                 _id: post.getId(),
@@ -57,10 +62,11 @@ export class RateAPI {
             await this.dbi.update({ owner: this.uid }, { posts: this.userRatedPosts.posts });
             post.setDislikes(post.getDislikes() + 1);
             await post.flush();
+            return true;
         }
     }
 
-    async unratePost(post: BoundPost): Promise<void> {
+    async unratePost(post: BoundPost): Promise<boolean> {
         const ratedPost = this.userRatedPosts.posts.find(p => p._id.toHexString() === post.id);
         if (ratedPost) {
             if (ratedPost.rating === -1)
@@ -70,7 +76,8 @@ export class RateAPI {
             await post.flush();
             this.userRatedPosts.posts.splice(this.userRatedPosts.posts.indexOf(ratedPost), 1);
             await this.dbi.update({ owner: this.uid }, { posts: this.userRatedPosts.posts });
-        }
+            return true;
+        } else return false;
     }
 
 }
