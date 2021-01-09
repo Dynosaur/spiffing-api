66a2455f94243358726eac572d20e2f3a2fcd42b
Dynosaur <heracassia@gmail.com>
2021-01-09 14:37:16 -0600

    Created new jest project for api router tests

--- jest.config.js
+++ jest.config.js
@@ -16,6 +16,7 @@ module.exports = {
         createProject('unit-tests', 'unit'),
         createProject('unit-tests/mock', 'unit/mock-tests'),
         createProject('unit-tests/router', 'unit/router-tests'),
+        createProject('unit-tests/router/api', 'unit/router-tests/api-router-tests'),
         createProject('unit-tests/database', 'unit/database-tests'),
         createProject('validation-tests', 'validation')
     ]
