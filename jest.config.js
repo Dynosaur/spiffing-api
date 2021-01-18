9998cb4341c5605352dad688b35c2731d20b237a
Dynosaur <heracassia@gmail.com>
2021-01-17 18:28:26 -0600

    Simplified serial projects

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,25 @@
 const { compilerOptions } = require('./tsconfig');
 const { pathsToModuleNameMapper } = require('ts-jest/utils');
 
-function createProject(name, path, other) {
+function createProject(name, path, serial = false, other) {
     return {
         displayName: name,
         moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
         testMatch: [`<rootDir>/tests/${path}/**/*.ts`],
         transform: { '^.+\\.(ts|tsx)$': 'ts-jest' },
+        ...serial && { runner: 'jest-serial-runner' },
         ...other
     }
 }
 
 module.exports = {
     projects: [
-        createProject('integration-tests', 'integration'),
+        createProject('integration-tests', 'integration', true),
         createProject('unit-tests', 'unit'),
         createProject('unit-tests/mock', 'unit/mock-tests'),
         createProject('unit-tests/router', 'unit/router-tests'),
-        createProject('unit-tests/router/api', 'unit/router-tests/api-router-tests', { runner: 'jest-serial-runner' }),
+        createProject('unit-tests/router/api', 'unit/router-tests/api-router-tests', true),
         createProject('unit-tests/database', 'unit/database-tests'),
-        createProject('validation-tests', 'validation')
+        createProject('validation-tests', 'validation', true)
     ]
 };
