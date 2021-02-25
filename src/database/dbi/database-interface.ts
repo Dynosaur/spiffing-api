33e7b6f321194067658eb8f45b3ee1d6a628a355
Dynosaur <heracassia@gmail.com>
2021-02-25 14:12:18 -0600

    Changed partial<T> to filterquery<T>

--- src/database/dbi/database-interface.ts
+++ src/database/dbi/database-interface.ts
@@ -1,4 +1,4 @@
-import { Collection, ObjectId } from 'mongodb';
+import { Collection, FilterQuery, ObjectId } from 'mongodb';
 
 export interface Identifiable {
     _id: ObjectId;
@@ -12,18 +12,18 @@ export class DatabaseInterface<T extends Identifiable> {
         await this.collection.insertOne(data as any);
     }
 
-    async delete(query: Partial<T>): Promise<void> {
+    async delete(query: FilterQuery<T>): Promise<void> {
         await this.collection.deleteMany(query);
     }
 
-    async read(query: Partial<T>): Promise<T[]> {
+    async read(query: FilterQuery<T>): Promise<T[]> {
         const cursor = this.collection.find<T>(query);
         const items: T[] = [];
         await cursor.forEach(item => items.push(item));
         return items;
     }
 
-    async update(query: Partial<T>, updates: Partial<T>): Promise<{ matched: number; modified: number; }> {
+    async update(query: FilterQuery<T>, updates: Partial<T>): Promise<{ matched: number; modified: number; }> {
         const updateRequest = await this.collection.updateMany(query, { $set: updates });
         return {
             matched: updateRequest.matchedCount,
