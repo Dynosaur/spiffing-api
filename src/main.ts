bdba5abfad47735c11bd859b1891d91ec30dac9f
Dynosaur <heracassia@gmail.com>
2021-03-08 13:20:06 -0600

    Environment variable check before server starts

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,17 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
+if (process.env.PORT === undefined)
+    process.env.PORT = '47680';
+if (process.env.environment === undefined)
+    throw new Error('.env did not specify an \'environment\' field, please set to "PROD", "DEV", or "TEST".');
+if (process.env.DB_URL === undefined)
+    throw new Error('.env did not specify a \'DB_URL\' field.');
+if (process.env.KEY === undefined)
+    throw new Error('.env did not specify a \'KEY\' field.');
+
 const server = new Server();
-server.start(parseInt(process.env.PORT!)).catch(error => {
-    console.log(error); // eslint-disable-line
+server.start(parseInt(process.env.PORT)).catch(error => {
+    console.log(error); // eslint-disable-line no-console
     process.exit(1);
 });
