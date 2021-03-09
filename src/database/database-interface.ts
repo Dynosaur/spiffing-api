bd3442c50d8422549dc6c53ec0a5bf92027f10b4
Dynosaur <heracassia@gmail.com>
2021-03-09 14:21:06 -0600

    Improved error logging

--- src/database/database-interface.ts
+++ src/database/database-interface.ts
@@ -12,7 +12,7 @@ export class DatabaseInterface<T> {
     private name: string;
 
     constructor(private collection: Collection<T>) {
-        this.name = collection.collectionName;
+        this.name = collection.namespace;
     }
 
     getName(): string {
@@ -22,22 +22,23 @@ export class DatabaseInterface<T> {
     async create(data: OptionalId<T>): Promise<WithId<T>> {
         const insertOp = await this.collection.insertOne(data);
         if (insertOp.insertedCount !== 1)
-            throw new Error(`${this.collection.collectionName}: insert failed:
-             insertedCount: ${insertOp.insertedCount}`);
+            throw new Error(`(${this.name}) insert failed:\n\t` +
+            `insertedCount: ${insertOp.insertedCount}`);
         return insertOp.ops[0];
     }
 
     async delete(query: FilterQuery<T>): Promise<void> {
         const delOp = await this.collection.deleteOne(query);
         if (delOp.deletedCount !== 1)
-            throw new Error(`${this.collection.collectionName}: delete failed:
-             deleteCount: ${delOp.deletedCount}`);
+            throw new Error(`(${this.name}) delete failed:\n\t` +
+            `deleteCount: ${delOp.deletedCount}`);
     }
 
-    async deleteMany(query: FilterQuery<T>): Promise<number> {
+    async deleteMany(query: FilterQuery<T>, atLeast1 = true): Promise<number> {
         const delOp = await this.collection.deleteMany(query);
-        if (delOp.deletedCount === 0)
-            throw new Error(`(${this.name}) deleteMany failed: deleteCount: ${delOp.deletedCount}`);
+        if (atLeast1 && delOp.deletedCount === 0)
+            throw new Error(`(${this.name}) deleteMany failed:\n\t` +
+            `deleteCount: ${delOp.deletedCount}`);
         return delOp.deletedCount!;
     }
 
@@ -53,16 +54,20 @@ export class DatabaseInterface<T> {
     async updateOne(query: FilterQuery<T>, updates: UpdateQuery<T>, options?: UpdateOneOptions): Promise<void> {
         const upOp = await this.collection.updateOne(query, updates, options);
         if (upOp.matchedCount !== 1)
-            throw new Error(`(${this.name}) updateOne failed: matchedCount: ${upOp.matchedCount}`);
+            throw new Error(`(${this.name}) updateOne failed:\n\t` +
+            `matchedCount: ${upOp.matchedCount}`);
         if (upOp.modifiedCount !== 1)
-            throw new Error(`(${this.name}) updateOne failed: modifiedCount: ${upOp.modifiedCount}`);
+            throw new Error(`(${this.name}) updateOne failed:\n\t` +
+            `modifiedCount: ${upOp.modifiedCount}`);
     }
 
     async updateMany(query: FilterQuery<T>, updates: UpdateQuery<T>, options?: UpdateManyOptions): Promise<void> {
         const upOp = await this.collection.updateMany(query, updates, options);
         if (upOp.matchedCount === 0)
-            throw new Error(`(${this.name}) updateOne failed: matchedCount: ${upOp.matchedCount}`);
+            throw new Error(`(${this.name}) updateOne failed:\n\t` +
+            `matchedCount: ${upOp.matchedCount}`);
         if (upOp.modifiedCount === 0)
-            throw new Error(`(${this.name}) updateOne failed: modifiedCount: ${upOp.modifiedCount}`);
+            throw new Error(`(${this.name}) updateOne failed:\n\t` +
+            `modifiedCount: ${upOp.modifiedCount}`);
     }
 }
