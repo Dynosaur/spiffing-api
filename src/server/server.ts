95eaf939b106b06e574b32e7c4a99176f7079aa5
Dynosaur <heracassia@gmail.com>
2021-03-09 14:52:48 -0600

    Using an express router for api routes

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,24 +1,28 @@
-import cors from 'cors';
-import express from 'express';
-import { chalk } from 'tools/chalk';
-import { devInfo } from 'dev/dev-actions';
-import { randomBytes } from 'crypto';
-import { MongoClient } from 'database/mongo-client';
-import { DbRatedPosts } from 'database/rate';
-import { RouteRegister } from 'server/routing';
-import { CommonActions } from 'database/common-actions';
-import { prettyTimestamp } from 'tools/time';
-import { DbPost, PostAPI } from 'database/post';
-import { DbUser, UserAPI } from 'database/user';
-import { DatabaseActions } from 'route-handling/route-infra';
-import { DatabaseInterface } from 'database/database-interface';
-import { routes as apiRoutes } from 'router/api-router';
-import { executeRouteHandler } from 'route-handling/route-handler';
+import cors                     from 'cors';
+import { randomBytes }          from 'crypto';
+import express                  from 'express';
 import { Server as NodeServer } from 'http';
-import { routes as miscRoutes } from 'router/misc-router';
-import { routes as authRoutes } from 'router/auth-router';
+import { resolve }              from 'path';
 import { CommentAPI, DbComment } from 'database/comment';
-import { resolve } from 'path';
+import { CommonActions }         from 'database/common-actions';
+import { DatabaseInterface }     from 'database/database-interface';
+import { MongoClient }           from 'database/mongo-client';
+import { DbPost, PostAPI }       from 'database/post';
+import { DbRatedPosts }          from 'database/rate';
+import { DbUser, UserAPI }       from 'database/user';
+import { devInfo }               from 'dev/dev-actions';
+import { RouteRegister }         from 'server/routing';
+import { chalk }                 from 'tools/chalk';
+import { prettyTimestamp }       from 'tools/time';
+import { routes as apiRoutes }   from 'router/api-router';
+import { executeRouteHandler }   from 'route-handling/route-handler';
+import { routes as miscRoutes }  from 'router/misc-router';
+import { routes as authRoutes }  from 'router/auth-router';
+import { routes }                from 'router/route-map';
+import {
+    DatabaseActions,
+    isHandlerRoute
+} from 'route-handling/route-infra';
 
 export type HttpMethod = 'GET' | 'POST' | 'DELETE' | 'PATCH';
 
@@ -41,12 +45,10 @@ export class Server {
 
     requestFingerprintSize = 3;
 
-    constructor(private verbose = true) { }
+    constructor(private verbose = true) {}
 
     async initialize(): Promise<void> {
-        if (this.verbose) {
-            chalk.cyan('Starting server');
-        }
+        if (this.verbose) chalk.cyan('Starting server');
 
         let dbUri: string;
         let dbName: string;
@@ -64,20 +66,21 @@ export class Server {
                 dbName = 'spiffing';
                 break;
             default:
-                throw new Error(`process.env.environment key is ${process.env.environment}, please use "DEV", "PROD", or "TEST".`);
+                throw new Error(`process.env.environment key is ${process.env.environment},` +
+                'please use "DEV", "PROD", or "TEST".');
         }
 
-        if (!process.env.KEY || !/[a-f\d]{32}/.test(process.env.KEY!)) {
-            throw new Error(`Expected process.env.KEY to be a string of 64 bits in hexadecimal. Received: ${process.env.KEY} of type ${typeof process.env.KEY}`);
-        }
+        if (!process.env.KEY || !/[a-f\d]{32}/.test(process.env.KEY!))
+            throw new Error('Expected process.env.KEY to be a string of 64 bits in hexadecimal.' +
+            `Received: ${process.env.KEY} of type ${typeof process.env.KEY}`);
 
         this.mongo = new MongoClient(dbUri, dbName, this.verbose);
         await this.mongo.initialize();
 
-        this.userDbi = new DatabaseInterface<DbUser>(this.mongo.getCollection('users'));
-        this.postDbi = new DatabaseInterface<DbPost>(this.mongo.getCollection('posts'));
-        this.rateDbi = new DatabaseInterface<DbRatedPosts>(this.mongo.getCollection('rated'));
-        this.commentDbi = new DatabaseInterface<DbComment>(this.mongo.getCollection('comments'));
+        this.userDbi = new DatabaseInterface(this.mongo.getCollection('users'));
+        this.postDbi = new DatabaseInterface(this.mongo.getCollection('posts'));
+        this.rateDbi = new DatabaseInterface(this.mongo.getCollection('rated'));
+        this.commentDbi = new DatabaseInterface(this.mongo.getCollection('comments'));
 
         this.commentApi = new CommentAPI(this.commentDbi, this.postDbi);
         this.postApi = new PostAPI(this.postDbi, this.commentDbi);
@@ -98,21 +101,30 @@ export class Server {
         this.app.use(express.urlencoded({ extended: true }));
         this.app.use(express.static('spiffing'));
 
-        this.app.use((request, response, next) => {
+        const apiRouter = express.Router();
+        apiRouter.use((request, response) => {
             const fingerprint = randomBytes(this.requestFingerprintSize).toString('hex');
-            if (this.verbose) chalk.yellow(`${fingerprint} ${prettyTimestamp()} ${request.method} ${request.url}`);
+            if (this.verbose)
+                chalk.yellow(`${fingerprint} ${prettyTimestamp()} ${request.method} ${request.url}`);
             const info = this.routeRegister.isRegistered(request);
             if (info) {
-                if (info.streamHandler) info.streamHandler(request, this.verbose, fingerprint);
-                else executeRouteHandler(request, this.actions, info.handler, fingerprint, this.verbose);
-            } else next();
+                if (isHandlerRoute(info))
+                    executeRouteHandler(request, this.actions, info.handler, fingerprint, this.verbose);
+                else info.streamHandler(request, this.verbose, fingerprint);
+            } else {
+                response.status(404).json({ message: 'Path not supported.' });
+                if (this.verbose) chalk.yellow(`${fingerprint} Path is not supported.\n`);
+            }
         });
+
+        this.app.use('/api', apiRouter);
+        this.app.use('', (req, res) => res.sendFile(resolve(__dirname, '../../spiffing/index.html')));
 
         apiRoutes.forEach(info => this.routeRegister.register(info.path, info.method, info));
         authRoutes.forEach(info => this.routeRegister.register(info.path, info.method, info));
         devInfo.forEach(info => this.routeRegister.register(info.path, info.method, info));
         miscRoutes.forEach(info => this.routeRegister.register(info.path, info.method, info));
-        this.app.use('', (req, res) => res.sendFile(resolve(__dirname, '../../spiffing/index.html')));
+        routes.forEach(info => this.routeRegister.register(info.path, info.method, info));
     }
 
     async start(port: number): Promise<void> {
