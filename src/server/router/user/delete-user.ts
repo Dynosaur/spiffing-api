7ffa7518e535c0f7628eb2bc907ecab971c9104d
Dynosaur <heracassia@gmail.com>
2021-03-15 09:03:10 -0500

    Imported interface, fixed imports

--- src/server/router/user/delete-user.ts
+++ src/server/router/user/delete-user.ts
@@ -1,19 +1,42 @@
+import { DatabaseActions, HandlerRoute, RoutePayload } from 'route-handling/route-infra';
+import { IUnauthenticated, Unauthenticated } from 'interface/error/unauthenticated';
+import { IUnauthorized, Unauthorized } from 'interface/error/unauthorized';
+import { IAuthorizationParse } from 'interface/error/authorization-parse';
+import { IObjectIdParse } from 'interface/error/object-id-parse';
 import { Request } from 'express';
-import { IDeleteUser }                             from 'interface/responses/auth-endpoints';
-import { DeleteUser }                              from 'interface-bindings/auth-responses';
-import { UnauthenticatedError, UnauthorizedError } from 'interface-bindings/error-responses';
-import { DatabaseActions, RoutePayload }           from 'route-handling/route-infra';
-import { decodeBasicAuth }                         from 'tools/auth';
+import { decodeBasicAuth } from 'tools/auth';
 
+export namespace IDeleteUser {
+    export type ErrTx =
+        | IAuthorizationParse
+        | IObjectIdParse
+        | IUnauthenticated
+        | IUnauthorized;
 
-export async function deleteUser(request: Request, actions: DatabaseActions): Promise<RoutePayload<IDeleteUser.Tx>> {
-    if (!request.headers.authorization) return new UnauthenticatedError();
+    export interface Success {
+        ok: true;
+    }
 
-    const decodeAttempt = decodeBasicAuth(request.headers.authorization);
-    if (decodeAttempt instanceof RoutePayload) return decodeAttempt;
-    const user = await actions.common.authorize(decodeAttempt.username, decodeAttempt.password);
-    if (!user) return new UnauthorizedError();
+    export type Tx = ErrTx | Success;
+}
+
+type ReturnType = Promise<RoutePayload<IDeleteUser.Tx>>
+export async function deleteUser(request: Request, actions: DatabaseActions): ReturnType {
+    if (!request.headers.authorization) return new Unauthenticated();
+
+    const decode = decodeBasicAuth(request.headers.authorization);
+    if (decode.ok === false) return decode.error;
+    const user = await actions.common.authorize(decode.username, decode.password);
+    if (!user) return new Unauthorized();
 
     await actions.user.delete(user._id);
-    return new DeleteUser.Success(user);
+    return {
+        code: 200,
+        message: `Deleted user ${user.username} (${user.id}).`,
+        payload: { ok: true }
+    };
 }
+
+export const route: HandlerRoute = {
+    handler: deleteUser, method: 'DELETE', path: '/user'
+};
