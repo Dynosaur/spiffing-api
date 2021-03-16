cea4c8114d60c7d8b01c8271a2dac860f6a250d4
Dynosaur <heracassia@gmail.com>
2021-03-15 21:10:30 -0500

    Linted

--- src/tools/object-id.ts
+++ src/tools/object-id.ts
@@ -1,25 +1,25 @@
 import { ObjectId } from 'mongodb';
-import { ObjectIdParseError } from 'interface-bindings/error-responses';
+import { ObjectIdParse } from 'interface/error/object-id-parse';
 
 const errorMessage = 'Argument passed in must be a single String of 12 bytes' +
                      ' or a string of 24 hex characters';
 
 type ReturnType =
     | { ok: true; id: ObjectId; }
-    | { ok: false; error: ObjectIdParseError }
+    | { ok: false; error: ObjectIdParse }
 
-export function parseObjectId(id: string): ReturnType {
+export function parseObjectId(context: string, id: string): ReturnType {
     if (id === undefined || id === null) return {
-        ok: false,
-        error: new ObjectIdParseError(id)
+        error: new ObjectIdParse(context, id),
+        ok: false
     };
     try {
         const objectId = new ObjectId(id);
-        return { ok: true, id: objectId };
+        return { id: objectId, ok: true };
     } catch (e) {
         if (e.message === errorMessage) return {
-            ok: false,
-            error: new ObjectIdParseError(id)
+            error: new ObjectIdParse(context, id),
+            ok: false
         };
         else throw e;
     }
